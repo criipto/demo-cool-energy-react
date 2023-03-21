60ac1b2505bc77794686c0f17e6cc9ee48b6c093
nmoskaleva <natalia.moskaleva@criipto.com>
2023-03-21 17:11:36 +0100

    Add scroll to login screen for mobile

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -4,7 +4,7 @@ import { useCriiptoVerify, AuthMethodSelector } from '@criipto/verify-react';
 import '@criipto/verify-react/dist/criipto-verify-react.css';
 
 function Login() {
-  const [showHeader, setShowHeader] = useState(true);
+  const [showHeading, setShowHeading] = useState(true);
   const { error } = useCriiptoVerify();
 
   const location = useLocation();
@@ -15,7 +15,7 @@ function Login() {
 
   useEffect(() => {
     const handleResize = () => {
-      setShowHeader(window.matchMedia('(max-width: 1023px)').matches);
+      setShowHeading(window.matchMedia('(max-width: 767px)').matches);
     };
 
     handleResize();
@@ -43,37 +43,39 @@ function Login() {
 
   return (
     <div className="flex flex-col justify-end md:flex-col-reverse h-full md:h-[90vh] bg-heroMobile bg-contain md:bg-hero bg-no-repeat bg-top md:bg-bottom items-center">
-      <div className="flex flex-col mt-8 lg:mt-0 justify-center align-bottom content-center md:w-[463px]">
-        {showHeader && (
-          <h3 className="font-medium text-lg mx-5 m-2 ml-4 pl-5 mt-6 lg:m-0 lg:mt-8 leading-normal">
-            Login to Cool Energy to see your consumption data
-          </h3>
-        )}
-        {error && (
-          <>
-            <div className="bg-error rounded mt-4 mx-5 pb-[26px] pt-5 max-w-[460px] py-5">
-              <div className="flex flex-row nowrap px-4">
-                <img
-                  src="/alert.png"
-                  alt="Error icon"
-                  className="relative top-0 left-0 h-5 w-5 mr-2"
-                />
-                <h5 className="font-semibold">
-                  There was an error logging in.
-                </h5>
+      <div className="flex flex-col mt-8 lg:mt-0 justify-center align-bottom content-center md:w-[463px] overflow-scroll">
+        <div className="overflow-scroll">
+          {showHeading && (
+            <h3 className="font-medium text-lg mx-5 m-2 ml-4 pl-5 mt-60 leading-normal">
+              Login to Cool Energy to see your consumption data
+            </h3>
+          )}
+          {error && (
+            <>
+              <div className="bg-error rounded mt-4 mx-5 pb-[26px] pt-5 max-w-[460px] py-5">
+                <div className="flex flex-row nowrap px-4">
+                  <img
+                    src="/alert.png"
+                    alt="Error icon"
+                    className="relative top-0 left-0 h-5 w-5 mr-2"
+                  />
+                  <h5 className="font-semibold">
+                    There was an error logging in.
+                  </h5>
+                </div>
+                <div className="font-medium mt-2 ml-11">
+                  <>
+                    <p>Please try again.</p>
+                    {console.log(error.error_description)}
+                  </>
+                </div>
               </div>
-              <div className="font-medium mt-2 ml-11">
-                <>
-                  <p>Please try again.</p>
-                  {console.log(error.error_description)}
-                </>
-              </div>
-            </div>
-          </>
-        )}
-        <AuthMethodSelector
-          acrValues={acrValues.length ? acrValues : undefined}
-        />
+            </>
+          )}
+          <AuthMethodSelector
+            acrValues={acrValues.length ? acrValues : undefined}
+          />
+        </div>
       </div>
     </div>
   );
